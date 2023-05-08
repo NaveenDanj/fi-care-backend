c924baf7ed3851bda615525d6eca2c6aac90b8c3
naveendanj <naveenhettiwaththa@gmail.com>
2023-05-08 20:41:41 +0530

    added service provider model

---
+++ models/serviceprovider.model.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ServiceProviderSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+    },
+
+    fullname: {
+      type: String,
+      required: false,
+    },
+
+    email: {
+      type: String,
+      required: false,
+    },
+
+    phone: {
+      type: String,
+      required: false,
+    },
+
+    description : {
+        type : String,
+        required : false
+    },
+
+    rate : {
+        type : Number,
+        required : false
+    },
+
+    photoUrl: {
+      type: String,
+      required: false,
+    },
+
+    password: {
+      type: String,
+      required: false,
+    },
+
+    phoneVerified: {
+      type: Boolean,
+      required: false,
+    },
+  },
+  { timestamps: true, strict: false }
+);
+
+const ServiceProvider = mongoose.model("ServiceProvider", ServiceProviderSchema);
+module.exports = ServiceProvider;
